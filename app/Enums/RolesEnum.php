9ff6591f88b65cbc9d200383b58090e5a5802b3b
Yarnnguyen <namnguyen7230@gmail.com>
2025-03-24 14:40:45 +0700

    Add Spatie Laravel Permission package and update seeder logic

---
+++ app/Enums/RolesEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum RolesEnum: string
+{
+    case Administrator = 'Administrator';
+    case Vendor = 'Vendor';
+    case User = 'User';
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(RoleSeeder::class);
+        // $this->call(PermissionSeeder::class);
+        $this->call(UserSeeder::class);
+        // $this->call(RolePermissionSeeder::class);
+        // $this->call(UserRoleSeeder::class);
     }
 }
